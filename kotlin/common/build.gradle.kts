7a0440ef6d6305f0c0de5a289ccb266c4b737104
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-02 17:13:30 +0300

    Update kotlin.common module settings

--- kotlin/common/build.gradle.kts
+++ kotlin/common/build.gradle.kts
@@ -1,9 +1,18 @@
 plugins {
-    id("java-library")
-    id("org.jetbrains.kotlin.jvm")
+    id(Plugins.javaLibrary)
+    id(Plugins.kotlinJvm)
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_7
-    targetCompatibility = JavaVersion.VERSION_1_7
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+tasks.test {
+    useJUnit()
+}
+
+dependencies {
+    testImplementation(Dependencies.jUnit)
+    testImplementation(Dependencies.truth)
 }

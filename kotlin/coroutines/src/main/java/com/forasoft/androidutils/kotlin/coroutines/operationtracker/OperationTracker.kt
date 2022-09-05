67ec93f60a7a968921ff6f5030270676687a4b08
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-05 14:03:05 +0300

    Write documentation for OperationTracker

--- kotlin/coroutines/src/main/java/com/forasoft/androidutils/kotlin/coroutines/operationtracker/OperationTracker.kt
+++ kotlin/coroutines/src/main/java/com/forasoft/androidutils/kotlin/coroutines/operationtracker/OperationTracker.kt
@@ -6,24 +6,47 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
 import java.util.*
 
+/**
+ * Tracks ongoing operations started with [track] and uses [OperationKey] to distinguish
+ * between them.
+ *
+ * Use [isOperationOngoing] and [ongoingOperationKeys] to check if the operation is
+ * ongoing or not.
+ */
 class OperationTracker {
 
     private val ongoingOperations = MutableStateFlow(setOf<Operation>())
+
+    /**
+     * [Flow] that contains [Set] of [OperationKey]s of the ongoing operations.
+     */
     val ongoingOperationKeys = ongoingOperations
         .map {
             it.map { request -> request.key }.toSet()
         }
 
-    suspend fun <R : Any> track(key: OperationKey, body: suspend () -> R): R {
+    /**
+     * Runs [operation], tracks its status and returns the result. [key] used to
+     * distinguish the given operation from others.
+     *
+     * @param key [OperationKey] used to distinguish the given operation.
+     * @param operation operation to track.
+     * @return result of the [operation].
+     */
+    suspend fun <R : Any> track(key: OperationKey, operation: suspend () -> R): R {
         val request = Operation(UUID.randomUUID().toString(), key)
         try {
             ongoingOperations.update { it + request }
-            return body()
+            return operation()
         } finally {
             ongoingOperations.update { it - request }
         }
     }
 
+    /**
+     * Returns [Flow] that indicates if there is **at least one** ongoing operation with
+     * a key from the given [keys].
+     */
     fun isOperationOngoing(vararg keys: OperationKey): Flow<Boolean> {
         val keySet = keys.toSet()
         return ongoingOperationKeys.map { ongoingKeys ->

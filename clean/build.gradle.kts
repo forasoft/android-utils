c45f7cd1a4f291e83311c4779ef3b2fc1bbfe235
Artem Sidorov <s.artem.ekb@gmail.com>
2022-09-06 11:04:23 +0300

    Refactor clean module Gradle settings

--- clean/build.gradle.kts
+++ clean/build.gradle.kts
@@ -11,6 +11,16 @@ detekt {
     config = files("../config/detekt-config.yml")
 }
 
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
+}
+
 android {
     namespace = "com.forasoft.androidutils.clean"
     compileSdk = 33
@@ -47,14 +57,4 @@ android {
 dependencies {
     implementation(Dependencies.coroutines)
     implementation(Dependencies.timber)
-}
-
-tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
-    reports {
-        html.required.set(true)
-        xml.required.set(false)
-        txt.required.set(false)
-        sarif.required.set(false)
-        md.required.set(false)
-    }
 }

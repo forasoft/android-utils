73c1386b6f9c073d488239f737029df6fc1575c3
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-06 18:32:00 +0300

    Write documentation for Fade.kt functions

--- ui/view/src/main/java/com/forasoft/androidutils/ui/view/visibility/Fade.kt
+++ ui/view/src/main/java/com/forasoft/androidutils/ui/view/visibility/Fade.kt
@@ -5,21 +5,40 @@ import android.view.View
 import androidx.core.view.isVisible
 import kotlin.time.Duration
 
-// TODO: Write documentation
+/**
+ * Animates the appearance or disappearance of this [View] with fade effect depending on
+ * the [isVisible] value using the current state of the View as the initial state.
+ *
+ * @param isVisible indicates whether the View should appear or disappear with the fade animation.
+ * @param duration [Duration] of the animation. Defaults to
+ * [android.R.integer.config_shortAnimTime] value.
+ * @param fadeOutTargetVisibility visibility that will be set when the disappearing animation ends.
+ * Defaults to [View.GONE].
+ * @param interpolator [TimeInterpolator] that will be used by the underlying animator. Defaults
+ * to null that results in linear interpolation.
+ */
 fun View.fade(
     isVisible: Boolean,
     duration: Duration = this.context.getShortAnimationDuration(),
-    outTargetVisibility: Int = View.GONE,
+    fadeOutTargetVisibility: Int = View.GONE,
     interpolator: TimeInterpolator? = null,
 ) {
     if (isVisible) {
         this.fadeIn(duration, interpolator)
     } else {
-        this.fadeOut(duration, outTargetVisibility, interpolator)
+        this.fadeOut(duration, fadeOutTargetVisibility, interpolator)
     }
 }
 
-// TODO: Write documentation
+/**
+ * Animates the appearance of this [View] with fade-in effect using the current state of the
+ * View as the initial state.
+ *
+ * @param duration [Duration] of the animation. Defaults to
+ * [android.R.integer.config_shortAnimTime] value.
+ * @param interpolator [TimeInterpolator] that will be used by the underlying animator. Defaults
+ * to null that results in linear interpolation.
+ */
 fun View.fadeIn(
     duration: Duration = this.context.getShortAnimationDuration(),
     interpolator: TimeInterpolator? = null,
@@ -32,7 +51,17 @@ fun View.fadeIn(
         .alpha(1f)
 }
 
-// TODO: Write documentation
+/**
+ * Animates the disappearance of this [View] with fade-out effect using the current state of the
+ * View as the initial state.
+ *
+ * @param duration [Duration] of the animation. Defaults to
+ * [android.R.integer.config_shortAnimTime] value.
+ * @param targetVisibility visibility that will be set when the animation ends. Defaults to
+ * [View.GONE].
+ * @param interpolator [TimeInterpolator] that will be used by the underlying animator. Defaults
+ * to null that results in linear interpolation.
+ */
 fun View.fadeOut(
     duration: Duration = this.context.getShortAnimationDuration(),
     targetVisibility: Int = View.GONE,

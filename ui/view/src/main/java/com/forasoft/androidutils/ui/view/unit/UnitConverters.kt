6a2f2461e4970785ea5f664a1baa977d86098f6a
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-06 14:31:16 +0300

    Add UnitConverters.kt

---
+++ ui/view/src/main/java/com/forasoft/androidutils/ui/view/unit/UnitConverters.kt
@@ -0,0 +1,23 @@
+package com.forasoft.androidutils.ui.view.unit
+
+import android.content.Context
+
+/**
+ * Converts the given value in dp unit to an equivalent value in pixels.
+ */
+fun Context.dpToPx(dp: Int): Float = dp * resources.displayMetrics.density
+
+/**
+ * Converts the given value in dp unit to an equivalent value in pixels.
+ */
+fun Context.dpToPx(dp: Float): Float = dp * resources.displayMetrics.density
+
+/**
+ * Converts the given value in pixels to an equivalent value in dp unit.
+ */
+fun Context.pxToDp(px: Int): Float = px / resources.displayMetrics.density
+
+/**
+ * Converts the given value in pixels to an equivalent value in dp unit.
+ */
+fun Context.pxToDp(px: Float): Float = px / resources.displayMetrics.density

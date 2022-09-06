84b94515cf4463aac6da9c5b1faef59f41da77a0
Artem Sidorov <s.artem.ekb@gmail.com>
2022-09-06 11:02:50 +0300

    Add Detekt for webrtc module

--- webrtc/build.gradle.kts
+++ webrtc/build.gradle.kts
@@ -1,6 +1,24 @@
 plugins {
     id(Plugins.androidLibrary)
     id(Plugins.kotlinAndroid)
+
+    id(Plugins.detekt) version (Versions.detektPlugin)
+}
+
+detekt {
+    parallel = true
+    buildUponDefaultConfig = true
+    config = files("../config/detekt-config.yml")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
 }
 
 android {

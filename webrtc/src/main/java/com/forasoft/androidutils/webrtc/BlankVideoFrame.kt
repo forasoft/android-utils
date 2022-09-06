8ea0ed8bce1619ec832c897fe3a0a2a50cadcb5c
Dmitry Popov <dmitry.po@fora-soft.com>
2022-09-06 11:46:30 +0300

    Fix getBlankVideoFrame documentation

--- webrtc/src/main/java/com/forasoft/androidutils/webrtc/BlankVideoFrame.kt
+++ webrtc/src/main/java/com/forasoft/androidutils/webrtc/BlankVideoFrame.kt
@@ -17,7 +17,6 @@ private val BLACK_UV_BYTE = 0b10001000.toUByte().toByte()
  * @param width the width of the frame in pixels
  * @param height the height of the frame in pixels
  * @param rotation the rotation of the frame in degrees
- * @return
  */
 fun getBlankVideoFrame(width: Int, height: Int, rotation: Int): VideoFrame {
     val chromaHeight: Int = (height + 1) / 2

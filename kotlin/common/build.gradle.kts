ab5ce88befb39e4f7a8609e2a56a1e989e5e185c
Artem Sidorov <s.artem.ekb@gmail.com>
2022-09-06 11:00:27 +0300

    Fix Detekt configuration for kotlin modules

--- kotlin/common/build.gradle.kts
+++ kotlin/common/build.gradle.kts
@@ -8,7 +8,17 @@ plugins {
 detekt {
     parallel = true
     buildUponDefaultConfig = true
-    config = files("../config/detekt-config.yml")
+    config = files("../../config/detekt-config.yml")
+}
+
+tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+    reports {
+        html.required.set(true)
+        xml.required.set(false)
+        txt.required.set(false)
+        sarif.required.set(false)
+        md.required.set(false)
+    }
 }
 
 java {

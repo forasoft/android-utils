0a505a868c1cfc72784468e47fd0c8f33331b3c1
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-06 15:42:47 +0300

    Remove Text suffixes from Text subtypes

--- ui/view/src/main/java/com/forasoft/androidutils/ui/view/Text.kt
+++ ui/view/src/main/java/com/forasoft/androidutils/ui/view/Text.kt
@@ -12,13 +12,13 @@ sealed interface Text {
     /**
      * Returns a [String] value of the text.
      */
-    fun getString(context: Context): String
+    fun getString(context: Context): kotlin.String
 
     /**
      * Represents an empty text with a value of empty [String].
      */
     object Empty : Text {
-        override fun getString(context: Context): String = ""
+        override fun getString(context: Context): kotlin.String = ""
     }
 
     /**
@@ -27,19 +27,19 @@ sealed interface Text {
      * @property resourceId id of the resource string.
      * @property args parameters for the parametrized string.
      */
-    class ResourceText(
+    class Resource(
         @StringRes val resourceId: Int,
         private vararg val args: Any = emptyArray(),
     ) : Text {
 
-        override fun getString(context: Context): String {
+        override fun getString(context: Context): kotlin.String {
             @Suppress("SpreadOperator")
             return context.resources.getString(this.resourceId, *this.args)
         }
 
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
-            if (other !is ResourceText) return false
+            if (other !is Resource) return false
 
             if (resourceId != other.resourceId) return false
             if (!args.contentEquals(other.args)) return false
@@ -62,20 +62,20 @@ sealed interface Text {
      * @property count the number used to get the correct string for the plural rules.
      * @property args parameters for the parametrized string.
      */
-    class PluralResourceText(
+    class PluralsResource(
         @PluralsRes val resourceId: Int,
         private val count: Int,
         private vararg val args: Any = emptyArray(),
     ) : Text {
 
-        override fun getString(context: Context): String {
+        override fun getString(context: Context): kotlin.String {
             @Suppress("SpreadOperator")
             return context.resources.getQuantityString(this.resourceId, this.count, *this.args)
         }
 
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
-            if (other !is PluralResourceText) return false
+            if (other !is PluralsResource) return false
 
             if (resourceId != other.resourceId) return false
             if (count != other.count) return false
@@ -96,13 +96,13 @@ sealed interface Text {
     /**
      * Represents the plain [String] text.
      */
-    class StringText(private val text: String) : Text {
+    class String(private val text: kotlin.String) : Text {
 
-        override fun getString(context: Context): String = text
+        override fun getString(context: Context): kotlin.String = text
 
         override fun equals(other: Any?): Boolean {
             if (this === other) return true
-            if (other !is StringText) return false
+            if (other !is String) return false
 
             if (text != other.text) return false
 

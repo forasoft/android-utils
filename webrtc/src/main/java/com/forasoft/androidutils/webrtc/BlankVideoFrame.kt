3ad397f2682235e5d60d7d0f0367816ae3966823
Artem Sidorov <s.artem.ekb@gmail.com>
2022-09-06 10:54:16 +0300

    Add getBlankVideoFrame() function

---
+++ webrtc/src/main/java/com/forasoft/androidutils/webrtc/BlankVideoFrame.kt
@@ -0,0 +1,44 @@
+package com.forasoft.androidutils.webrtc
+
+import org.webrtc.JavaI420Buffer
+import org.webrtc.VideoFrame
+import java.nio.ByteBuffer
+
+/** This is a two half-bytes having half of their maximum value
+ *
+ * The UV parts of YUV420 are half-byte in size, hence they belong to the range [0b0000, 0b1111].
+ * We need the byte of two half-bytes which are in the middle of their range */
+private val BLACK_UV_BYTE = 0b10001000.toUByte().toByte()
+
+fun getBlankVideoFrame(width: Int, height: Int, rotation: Int): VideoFrame {
+    val chromaHeight: Int = (height + 1) / 2
+    val strideUV: Int = (width + 1) / 2
+
+    val yBuffer = ByteBuffer.allocateDirect(width * height)
+    repeat(width * height) { yBuffer.put(0.toUByte().toByte()) }
+    yBuffer.rewind()
+
+    val uBuffer = ByteBuffer.allocateDirect(strideUV * chromaHeight)
+    repeat(strideUV * chromaHeight) { uBuffer.put(BLACK_UV_BYTE) }
+    uBuffer.rewind()
+
+    val vBuffer = ByteBuffer.allocateDirect(strideUV * chromaHeight)
+    repeat(strideUV * chromaHeight) { vBuffer.put(BLACK_UV_BYTE) }
+    vBuffer.rewind()
+
+    return VideoFrame(
+        JavaI420Buffer.wrap(
+            width,
+            height,
+            yBuffer,
+            width,
+            uBuffer,
+            strideUV,
+            vBuffer,
+            strideUV,
+            null
+        ),
+        rotation,
+        System.currentTimeMillis()
+    )
+}

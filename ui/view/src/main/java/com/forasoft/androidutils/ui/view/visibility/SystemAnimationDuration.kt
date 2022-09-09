e0b81952b0748615fa09ae9498b149740da2d25f
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-09 16:20:23 +0300

    Write documentation for SystemAnimationDuration

--- ui/view/src/main/java/com/forasoft/androidutils/ui/view/visibility/SystemAnimationDuration.kt
+++ ui/view/src/main/java/com/forasoft/androidutils/ui/view/visibility/SystemAnimationDuration.kt
@@ -4,12 +4,22 @@ import android.content.Context
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
+/**
+ * Represents animation durations specified in system resources.
+ * The corresponding value can be retrieved with [Context.getSystemAnimationDuration] function.
+ */
 enum class SystemAnimationDuration {
     SHORT,
     MEDIUM,
     LONG,
 }
 
+/**
+ * Returns [Duration] of the system animation specified in system resources.
+ *
+ * @param duration [SystemAnimationDuration] that represents the system animation duration
+ * specified in system resources.
+ */
 fun Context.getSystemAnimationDuration(duration: SystemAnimationDuration): Duration {
     val resId = when (duration) {
         SystemAnimationDuration.SHORT -> android.R.integer.config_shortAnimTime

bf92329f971418585e1e4a0c358dd2a0eb941a8a
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-12 14:52:40 +0300

    Update ByteArray.toHexString() tests

--- kotlin/common/src/test/java/com/forasoft/androidutils/kotlin/common/ByteArrayKtTest.kt
+++ kotlin/common/src/test/java/com/forasoft/androidutils/kotlin/common/ByteArrayKtTest.kt
@@ -6,13 +6,22 @@ import org.junit.Test
 class ByteArrayKtTest {
 
     @Test
-    fun toHexString_returnsCorrectString() {
-        val byteArray = "Sample byte array".toByteArray()
-        val expected = byteArray.joinToString("") { "%02x".format(it) }
+    fun toHexString_whenByteArrayIsNotEmpty_returnsCorrectString() {
+        val byteArray = byteArrayOf(0, 1, 10, 100, 1000.toByte())
 
         val actual = byteArray.toHexString()
+        println(actual)
 
-        assertThat(actual).isEqualTo(expected)
+        assertThat(actual).isEqualTo("00 01 0A 64 E8")
+    }
+
+    @Test
+    fun toHexString_whenByteArrayIsEmpty_returnsEmptyString() {
+        val byteArray = byteArrayOf()
+
+        val hexString = byteArray.toHexString()
+
+        assertThat(hexString).isEmpty()
     }
 
 }

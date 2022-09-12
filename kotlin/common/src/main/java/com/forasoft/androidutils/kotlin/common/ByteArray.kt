9d82eb420136aeec1cb1321e1b25e67c6ac06bb7
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-12 14:52:34 +0300

    Update ByteArray.toHexString()

--- kotlin/common/src/main/java/com/forasoft/androidutils/kotlin/common/ByteArray.kt
+++ kotlin/common/src/main/java/com/forasoft/androidutils/kotlin/common/ByteArray.kt
@@ -2,7 +2,13 @@ package com.forasoft.androidutils.kotlin.common
 
 /**
  * Converts [ByteArray] to a Hex string.
+ *
+ * Example:
+ * ```
+ * byteArrayOf(0, 1, 10, 100).toHexString()
+ * > "00 01 0A 64"
+ * ```
  */
-fun ByteArray.toHexString(): String {
-    return this.joinToString(separator = "") { byte -> "%02x".format(byte) }
+fun ByteArray.toHexString(separator: String = " "): String {
+    return this.joinToString(separator) { byte -> "%02X".format(byte) }
 }

273c1ac2a71eb2b5124c990350c48ee82be23bbd
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-12 16:00:46 +0300

    Write documentation for Context.openBrowser()

--- platfrom/android/src/main/java/com/forasoft/androidutils/platform/android/Context.kt
+++ platfrom/android/src/main/java/com/forasoft/androidutils/platform/android/Context.kt
@@ -1,10 +1,17 @@
+@file:Suppress("unused")
+
 package com.forasoft.androidutils.platform.android
 
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 
-// TODO: Write documentation
+/**
+ * Opens the given [url] in the browser.
+ *
+ * @param url link to open.
+ * @return `true` if the browser has been successfully opened, `false` otherwise.
+ */
 fun Context.openBrowser(url: String): Boolean {
     val uri = Uri.parse(url)
     val intent = Intent(Intent.ACTION_VIEW, uri)

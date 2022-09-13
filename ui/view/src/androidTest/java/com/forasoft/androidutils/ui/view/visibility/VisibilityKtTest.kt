69b602a2a5e62cdccea94a62f34773098e359ff3
Artem Sidorov <artem.sid@fora-soft.com>
2022-09-13 14:34:36 +0300

    Rename tests

--- ui/view/src/androidTest/java/com/forasoft/androidutils/ui/view/visibility/VisibilityKtTest.kt
+++ ui/view/src/androidTest/java/com/forasoft/androidutils/ui/view/visibility/VisibilityKtTest.kt
@@ -11,7 +11,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class VisibilityTest {
+class VisibilityKtTest {
 
     private val context: Context
         get() = InstrumentationRegistry.getInstrumentation().targetContext

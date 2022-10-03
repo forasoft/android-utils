d91f3e13438800726ff8d8f02d11b8ae46611800
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-03 14:17:20 +0300

    Add LogAggregatorActivity

---
+++ logaggregator/src/main/java/com/forasoft/androidutils/logaggregator/LogAggregatorActivity.kt
@@ -0,0 +1,13 @@
+package com.forasoft.androidutils.logaggregator
+
+import android.app.Activity
+import android.os.Bundle
+
+class LogAggregatorActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_aggregator)
+    }
+
+}

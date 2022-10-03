39b0c5f451dfa41504cfffd791ba97b8cd1627da
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-03 10:43:13 +0300

    Add logaggregator module

---
+++ logaggregator/build.gradle.kts
@@ -0,0 +1,40 @@
+plugins {
+    id(Plugins.androidLibrary)
+    id(Plugins.kotlinAndroid)
+}
+
+android {
+    namespace = "com.forasoft.androidutils.logaggregator"
+    compileSdk = 33
+
+    defaultConfig {
+        minSdk = 16
+        targetSdk = 33
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    
+}

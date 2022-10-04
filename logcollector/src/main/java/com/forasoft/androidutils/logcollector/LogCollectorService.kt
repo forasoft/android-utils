08d2cb31e38ea53a3b9aaac0a1ccc675876eaada
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-04 18:29:50 +0300

    Do not start LogCollectorService as foreground

--- logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorService.kt
+++ logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorService.kt
@@ -1,12 +1,9 @@
 package com.forasoft.androidutils.logcollector
 
-import android.app.Notification
 import android.app.Service
+import android.content.ComponentName
 import android.content.Intent
 import android.os.IBinder
-import androidx.core.app.NotificationChannelCompat
-import androidx.core.app.NotificationCompat
-import androidx.core.app.NotificationManagerCompat
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.cancel
@@ -17,28 +14,22 @@ class LogCollectorService : Service() {
 
     private val coroutineScope = CoroutineScope(SupervisorJob())
 
-    private val notificationManager by lazy {
-        NotificationManagerCompat.from(this)
-    }
-
     private val logCollector by lazy { LogCollector(this) }
 
-    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_NOT_STICKY
+    // TODO
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int = START_STICKY
 
     override fun onBind(intent: Intent?): IBinder? = null
 
     override fun onCreate() {
         super.onCreate()
         Timber.v("LogCollectorService created")
-
-        startForeground()
         coroutineScope.launch { logCollector.start() }
     }
 
     override fun onDestroy() {
         super.onDestroy()
         Timber.v("LogCollectorService destroyed")
-
         coroutineScope.cancel()
     }
 
@@ -46,41 +37,6 @@ class LogCollectorService : Service() {
         super.onTaskRemoved(rootIntent)
         Timber.v("onTaskRemoved")
         stopSelf()
-    }
-
-    private fun startForeground() {
-        createNotificationChannel()
-        val notification = createNotification()
-        startForeground(NOTIFICATION_ID, notification)
-    }
-
-    private fun createNotificationChannel() {
-        val importance = NotificationManagerCompat.IMPORTANCE_LOW
-        val id = getString(R.string.log_collector_notification_channel_id)
-        val name = getString(R.string.log_collector_notification_channel_name)
-        val channel = NotificationChannelCompat.Builder(id, importance)
-            .setName(name)
-            .setLightsEnabled(false)
-            .setVibrationEnabled(false)
-            .build()
-        notificationManager.createNotificationChannel(channel)
-    }
-
-    private fun createNotification(): Notification {
-        val channelId = getString(R.string.log_collector_notification_channel_id)
-        val title = getString(R.string.log_collector_notification_title)
-        val appName = getString(applicationInfo.labelRes)
-        val body = getString(R.string.log_collector_notification_body, appName)
-        return NotificationCompat.Builder(this, channelId)
-            .setPriority(NotificationCompat.PRIORITY_LOW)
-            .setSmallIcon(android.R.drawable.ic_menu_save) // TODO: Change icon
-            .setContentTitle(title)
-            .setContentText(body)
-            .build()
-    }
-
-    companion object {
-        private const val NOTIFICATION_ID = 145543233
     }
 
 }
--- logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorServiceInitializer.kt
+++ logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorServiceInitializer.kt
@@ -2,7 +2,6 @@ package com.forasoft.androidutils.logcollector
 
 import android.content.Context
 import android.content.Intent
-import android.os.Build
 import androidx.startup.Initializer
 
 @Suppress("Unused")
@@ -10,11 +9,7 @@ class LogCollectorServiceInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
         val intent = Intent(context, LogCollectorService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            context.startForegroundService(intent)
-        } else {
-            context.startService(intent)
-        }
+        context.startService(intent)
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {

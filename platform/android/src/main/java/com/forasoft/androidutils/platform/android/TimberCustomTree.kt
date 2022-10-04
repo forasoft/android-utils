e3718a0d13dfa2985999d65de0e4b3b24fc9dd90
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-04 15:23:41 +0300

    Add Timber.plantCustomTree() for adding customizable logging trees

---
+++ platform/android/src/main/java/com/forasoft/androidutils/platform/android/TimberCustomTree.kt
@@ -0,0 +1,20 @@
+package com.forasoft.androidutils.platform.android
+
+import timber.log.Timber
+
+/**
+ * Adds a new customizable logging tree.
+ *
+ * @param onLog callback to be invoked for all Timber logs.
+ */
+@Suppress("Unused")
+inline fun Timber.Forest.plantCustomTree(
+    crossinline onLog: (priority: Int, tag: String?, message: String, throwable: Throwable?) -> Unit,
+) {
+    val tree = object : Timber.Tree() {
+        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
+            onLog(priority, tag, message, t)
+        }
+    }
+    this.plant(tree)
+}

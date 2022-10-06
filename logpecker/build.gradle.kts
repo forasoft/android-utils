b2354eb829e4611d9b17ef71cea230a85da9be1e
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-06 16:08:48 +0300

    Remove unused dependencies

--- logpecker/build.gradle.kts
+++ logpecker/build.gradle.kts
@@ -54,7 +54,6 @@ tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
 }
 
 dependencies {
-    implementation(Dependencies.jetpackCore)
     implementation(Dependencies.startup)
     implementation(Dependencies.coroutines)
     implementation(Dependencies.timber)

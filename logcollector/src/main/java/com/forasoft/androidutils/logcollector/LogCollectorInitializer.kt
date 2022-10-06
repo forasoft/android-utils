01ca86dac8a7ccea64ad2da52c2e54b688dcfbe3
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-06 14:07:20 +0300

    Rename LogCollectorServiceInitializer to LogCollectorInitializer

--- logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorInitializer.kt
+++ logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorInitializer.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.startup.Initializer
 
 @Suppress("Unused")
-class LogCollectorServiceInitializer : Initializer<Unit> {
+class LogCollectorInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
         val isEnabled = context.resources.getBoolean(R.bool.log_collector_is_enabled)

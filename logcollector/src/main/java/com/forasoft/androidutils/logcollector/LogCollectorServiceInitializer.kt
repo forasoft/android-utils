5e1ae049f224e66749c3e8343a2928f7801e41ee
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-06 14:06:30 +0300

    Use bool res value to enable or disable LogCollector

--- logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorServiceInitializer.kt
+++ logcollector/src/main/java/com/forasoft/androidutils/logcollector/LogCollectorServiceInitializer.kt
@@ -1,15 +1,13 @@
 package com.forasoft.androidutils.logcollector
 
 import android.content.Context
-import android.content.Intent
 import androidx.startup.Initializer
 
 @Suppress("Unused")
 class LogCollectorServiceInitializer : Initializer<Unit> {
 
     override fun create(context: Context) {
-        val intent = Intent(context, LogCollectorService::class.java)
-        context.startService(intent)
+        val isEnabled = context.resources.getBoolean(R.bool.log_collector_is_enabled)
     }
 
     override fun dependencies(): List<Class<out Initializer<*>>> {

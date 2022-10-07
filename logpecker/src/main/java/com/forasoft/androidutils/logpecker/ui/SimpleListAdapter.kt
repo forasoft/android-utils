92f39c487516467e79d2a9e434cf6578b1ebd57e
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-07 12:31:59 +0300

    Update SimpleListAdapter

--- logpecker/src/main/java/com/forasoft/androidutils/logpecker/ui/SimpleListAdapter.kt
+++ logpecker/src/main/java/com/forasoft/androidutils/logpecker/ui/SimpleListAdapter.kt
@@ -5,16 +5,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 
-class SimpleListAdapter<T>(
+internal class SimpleListAdapter<T>(
     private val itemLayoutResId: Int,
-    private val items: List<T>,
-    private val bindView: SimpleListAdapter<T>.(View, Int) -> Unit,
+    private val bindView: SimpleListAdapter<T>.(View, T) -> Unit,
 ) : BaseAdapter() {
+
+    private var items: List<T> = emptyList()
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val view = convertView ?: LayoutInflater.from(parent?.context)
             .inflate(itemLayoutResId, parent, false)!!
-        bindView(view, position)
+        val item = getItem(position)
+        bindView(view, item)
         return view
     }
 
@@ -23,5 +25,10 @@ class SimpleListAdapter<T>(
     override fun getItemId(position: Int): Long = position.toLong()
 
     override fun getCount(): Int = items.size
+
+    fun submitList(items: List<T>) {
+        this.items = items
+        notifyDataSetChanged()
+    }
 
 }

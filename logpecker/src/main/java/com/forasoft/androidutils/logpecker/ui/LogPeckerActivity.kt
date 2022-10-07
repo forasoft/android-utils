bb58b3e316216b5b5533f152ce73153780086fdb
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-07 11:42:37 +0300

    Add refresh button

--- logpecker/src/main/java/com/forasoft/androidutils/logpecker/ui/LogPeckerActivity.kt
+++ logpecker/src/main/java/com/forasoft/androidutils/logpecker/ui/LogPeckerActivity.kt
@@ -2,6 +2,7 @@ package com.forasoft.androidutils.logpecker.ui
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.ImageButton
 import android.widget.TextView
 import com.forasoft.androidutils.logpecker.R
 
@@ -10,14 +11,17 @@ internal class LogPeckerActivity : Activity() {
     private val title: TextView by lazy {
         findViewById(R.id.forasoftandroidutils_log_pecker_activity_title)
     }
+    private val refresh: ImageButton by lazy {
+        findViewById(R.id.forasoftandroidutils_log_pecker_activity_refresh)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.forasoftandroidutils_activity_log_pecker)
-        setUpViews()
+        setUpTitle()
     }
 
-    private fun setUpViews() {
+    private fun setUpTitle() {
         val appName = getString(applicationInfo.labelRes)
         title.text = getString(R.string.forasoftandroidutils_log_pecker_activity_title, appName)
     }

2cafd18bebcc6466b32dbdb2f9b4d7289e11d54d
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-14 11:47:43 +0300

    Update Gradle and AGP versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     const val kotlin = "1.7.20"
-    const val androidGradlePlugin = "7.3.0"
+    const val androidGradlePlugin = "7.3.1"
 
     const val detektPlugin = "1.21.0"
     const val checkDependencyUpdatesPlugin = "1.5.0"

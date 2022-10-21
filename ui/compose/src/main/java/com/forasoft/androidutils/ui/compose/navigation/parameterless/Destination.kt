9d4bbfec6af088bba11c2ab315e42d97dbf2d760
Artem Sidorov <artem.sid@fora-soft.com>
2022-10-21 14:15:10 +0300

    Add parameterless version of Destination

---
+++ ui/compose/src/main/java/com/forasoft/androidutils/ui/compose/navigation/parameterless/Destination.kt
@@ -0,0 +1,23 @@
+package com.forasoft.androidutils.ui.compose.navigation.parameterless
+
+import com.forasoft.androidutils.ui.compose.navigation.Destination
+
+/**
+ * An abstraction for a Compose Navigation destination that encapsulates destination's
+ * [routeSchema], [arguments] and [deepLinks].
+ *
+ * Parameterless version of [Destination].
+ */
+@Suppress("Unused")
+abstract class Destination : Destination<Unit>() {
+
+    abstract override val routeSchema: String
+
+    /**
+     * String that can be used as a route for NavController navigation.
+     */
+    val route: String get() = routeSchema
+
+    override fun createRoute(args: Unit): String = route
+
+}

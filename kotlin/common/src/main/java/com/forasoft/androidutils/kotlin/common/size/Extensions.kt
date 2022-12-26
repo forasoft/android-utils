74c8fdde2f16098f4ff982018dc64b28462902cc
Dmitry Popov <dmitry.po@fora-soft.com>
2022-12-26 10:31:24 +0300

    Add documentation

--- kotlin/common/src/main/java/com/forasoft/androidutils/kotlin/common/size/Extensions.kt
+++ kotlin/common/src/main/java/com/forasoft/androidutils/kotlin/common/size/Extensions.kt
@@ -23,6 +23,9 @@ fun Size.coerceAtMost(maximumValue: Size): Size {
     return if (this > maximumValue) maximumValue else this
 }
 
+/**
+ * Ensures that this value lies in the specified range [minimumValue]..[maximumValue]
+ */
 fun Size.coerceIn(minimumValue: Size, maximumValue: Size): Size {
     require(minimumValue <= maximumValue) { "Lower bound can't be bigger than upper bound" }
     return when {

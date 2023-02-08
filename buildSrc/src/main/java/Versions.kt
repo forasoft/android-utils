dd1862ad618317a8cf5077d25b86a45be24a4ed3
Artem Sidorov <artem.sid@fora-soft.com>
2023-02-08 11:57:34 +0300

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,19 +3,19 @@ object Versions {
     const val compileSdk = 31
     const val targetSdk = 31
 
-    const val androidGradlePlugin = "7.3.1"
-    const val detektPlugin = "1.21.0"
+    const val androidGradlePlugin = "7.4.1"
+    const val detektPlugin = "1.22.0"
     const val checkDependencyUpdatesPlugin = "1.5.0"
 
-    const val jetpackCore = "1.8.0"
-    const val appCompat = "1.4.2"
-    const val navigation = "2.5.2"
+    const val jetpackCore = "1.9.0"
+    const val appCompat = "1.6.0"
+    const val navigation = "2.5.3"
     const val startup = "1.1.1"
 
-    const val composeCompiler = "1.3.2"
-    const val compose = "1.1.1"
+    const val composeBom = "2023.01.00"
+    const val composeCompiler = "1.4.0"
 
-    const val kotlin = "1.7.20"
+    const val kotlin = "1.8.0"
     const val coroutines = "1.6.4"
 
     const val okHttp = "4.10.0"
@@ -24,9 +24,9 @@ object Versions {
     const val timber = "5.0.1"
 
     const val junit = "4.13.2"
-    const val androidJunit = "1.1.3"
-    const val espresso = "3.4.0"
+    const val androidJunit = "1.1.5"
+    const val espresso = "3.5.1"
     const val truth = "1.1.3"
-    const val robolectric = "4.9"
+    const val robolectric = "4.9.2"
 
 }

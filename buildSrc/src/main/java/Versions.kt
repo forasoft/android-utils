134d43fc7e56236a9d4ea2081457279bddc3686a
Artem Sidorov <artem.sid@fora-soft.com>
2023-05-10 18:45:42 +0300

    Update Jetpack versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -7,9 +7,9 @@ object Versions {
     const val detektPlugin = "1.22.0"
     const val checkDependencyUpdatesPlugin = "1.5.0"
 
-    const val jetpackCore = "1.8.0"
-    const val appCompat = "1.4.2"
-    const val navigation = "2.5.2"
+    const val jetpackCore = "1.10.0"
+    const val appCompat = "1.6.1"
+    const val navigation = "2.5.3"
     const val startup = "1.1.1"
 
     const val composeCompiler = "1.4.7"
@@ -24,8 +24,8 @@ object Versions {
     const val timber = "5.0.1"
 
     const val junit = "4.13.2"
-    const val androidJunit = "1.1.3"
-    const val espresso = "3.4.0"
+    const val androidJunit = "1.1.5"
+    const val espresso = "3.5.1"
     const val truth = "1.1.3"
     const val robolectric = "4.9"
 

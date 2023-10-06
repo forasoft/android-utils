ea01b9737813b3b4f7812c9eb44f0323f9b813a5
Alexander <alexander.pol@fora-soft.com>
2023-10-06 17:00:23 +0300

    Improve documentation

--- ui/compose/src/main/java/com/forasoft/androidutils/ui/compose/behavior/BehaviorController.kt
+++ ui/compose/src/main/java/com/forasoft/androidutils/ui/compose/behavior/BehaviorController.kt
@@ -80,21 +80,21 @@ public interface BehaviorController<T : Behavior> {
     public val currentBehavior: StateFlow<T>
 
     /**
-     * A method to set the default behavior.
+     * Sets the default behavior.
      *
      * @param behavior [Behavior] to be set as the default.
      */
     public fun setDefaultBehavior(behavior: T)
 
     /**
-     * A method to add a behavior to the stack.
+     * Adds a behavior to the stack.
      *
      * @param behavior [Behavior] to add to the stack.
      */
     public fun pushBehavior(behavior: T)
 
     /**
-     * A method to remove a behavior from the stack.
+     * Removes a behavior from the stack.
      *
      * @param behavior [Behavior] to remove from the stack.
      */
